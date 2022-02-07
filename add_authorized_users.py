e8b5e2384a5a0919410998f735989a5d8ef0d70a
“GrahamCBell” <GrahamCBell@cmail.carleton.ca>
2022-02-06 19:54:36 -0500

    added my email

--- add_authorized_users.py
+++ add_authorized_users.py
@@ -3,4 +3,6 @@ from mydbconfig import *
 backend = Backend(config, email, firstname, lastname)
 
 backend.add_authorized_users('boshenzhang@cmail.carleton.ca')
-backend.add_authorized_users('grahamcbell@cmail.carleton.ca')
+backend.add_authorized_users('grahamcarsonbell@gmail.com')
+backend.add_authorized_users('meiacopeland@cmail.carleton.ca')
+
--- dbconfig.py
+++ dbconfig.py
@@ -1,10 +1,10 @@
-email="firstnamelastname@cmail.carleton.ca'
-firstname="firstname"
-lastname="lastname"
+email="grahamcarsonbell@gmail.com'
+firstname="graham"
+lastname="bell"
 
 config = dict(
-    apiKey = "myapikey",
-    authDomain = "<myproject>.firebase.com",
-    databaseURL = "<myurl>.firebaseio.com/",
-    storageBucket = "<mystorage>.appspot.com"
+  "apiKey": "AIzaSyDA_s8XGscNq3BIoU8PSS2pU6htnQ73RJE",
+  "authDomain": "sandbox-99e4a.firebaseapp.com",
+  "databaseURL": "https://sandbox-99e4a-default-rtdb.firebaseio.com/",
+  "storageBucket": "sandbox-99e4a.appspot.com"
 )
